471b7496f9a39dc4d5df12f4d27aea251043339a
ibnughali <ibnughali19@gmail.com>
2023-04-06 15:12:21 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo "Nama saya Arya Wirayuda 
+            Saya tinggal di daerah jababeka
+            Olahraga yang saya sukai adalah 
+            Futsal";
+    }
+}
